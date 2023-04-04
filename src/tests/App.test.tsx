4de7e3797aa1bec65083e5524d2d1ac4d8e62271
NAlexH2 <alex@nah-red.net>
2023-04-03 20:53:02 -0700

    Template test file kept for reference.

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../TypeScriptDemo/App";
+
+/**
+ * This is sample test file provided by the template, use it to both do your
+ * own research and learn how to write tests for your specific section of the
+ * project. This test will actually passed due to the import of 'App' calling
+ * the template demo generate when making a new project.
+ *
+ * - Alex
+ */
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
