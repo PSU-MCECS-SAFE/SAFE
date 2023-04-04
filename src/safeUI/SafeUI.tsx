59fa1e39623c6000c4f2315d5f9bcb8f15755d15
NAlexH2 <alex@nah-red.net>
2023-04-03 20:52:40 -0700

    Simple sample ui example

---
+++ src/safeUI/SafeUI.tsx
@@ -0,0 +1,18 @@
+//This import isn't required in newer versions of react in every file, but
+//is a fail safe for older versions. Best to do it anyways!
+import React from "react";
+
+/* This doesn't do a lot currently, but if you type `npm start` this *should*
+ * display just this message. From there you are free to edit and make changes,
+ * then save to see them adjust in realtime.
+ */
+
+function SafeUI() {
+  return (
+    <div>
+      <h1>This is SAFE - System for Anonymous Feedback!</h1>
+    </div>
+  );
+}
+
+export default SafeUI;
