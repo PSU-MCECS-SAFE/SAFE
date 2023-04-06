6b67853edbd8c0eac21e5df2f7c3e26ddbc5eaa0
NAlexH2 <alex@nah-red.net>
2023-04-05 19:59:42 -0700

    Added tests for the constructor

--- src/tests/Message.test.tsx
+++ src/tests/Message.test.tsx
@@ -1,3 +1,63 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import Message from "../safeMessage/SafeMessage";
+
+//Demo object to use in validation
+const testObject: Message = new Message(
+    "Test Title",
+    "Test Receiver",
+    "Test String"
+);
+
+
+//Validate the constructor generates an object correctly
+test("Validation of message constructor", () => {
+    expect(testObject.title).toBe("Test Title");
+    expect(testObject.receiver_name).toBe("Test Receiver");
+    expect(testObject.message).toBe("Test String");
+});
+
+
+// Checking if error is thrown if title is empty in several flavors.
+test("Constructor throws if `title` is empty", () => {
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("", "", "");
+    }).toThrowError("The title cannot be empty.");
+
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("", "Test Rcvr", "");
+    }).toThrowError("The title cannot be empty.");
+
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("", "Test Rcvr", "Test Str");
+    }).toThrowError("The title cannot be empty.");
+
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("", "", "Test Str");
+    }).toThrowError("The title cannot be empty.");
+});
+
+
+// Checking if error is thrown if receiver_name is empty in several flavors.
+test("Constructor throws if `receiver_name` is empty", () => {
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("Test Title", "", "");
+    }).toThrowError("The intended Receiver cannot be empty.");
+
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("Test Title", "", "Test Str");
+    }).toThrowError("The intended Receiver cannot be empty.");
+});
+
+
+// Checking if error is thrown if message is empty. Only one possible test here!
+test("Constructor throws if `message` is empty", () => {
+    expect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const assertTitleNull: Message = new Message("Test Title", "Test Rcvr", "");
+    }).toThrowError("The message cannot be empty.");
+});
