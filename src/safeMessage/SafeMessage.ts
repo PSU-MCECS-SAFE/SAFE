09a5cdc58bc2672e0b0400240c56d3d7fb92bada
dylanconklin <dconklin@pdx.edu>
2023-04-09 16:41:25 -0700

    Added code field, reply status, read status, and time stamps to Message object

--- src/safeMessage/SafeMessage.ts
+++ src/safeMessage/SafeMessage.ts
@@ -6,7 +6,10 @@ export class Message {
   title: string;
   receiver_name: string;
   message: string;
-
+  code: string;
+  receive_reply: boolean;
+  has_been_read : boolean;
+  time_submitted : Date;
 
   /**
    * Constructor for the `Message` class object
@@ -14,14 +17,26 @@ export class Message {
    * @param receiver_name - Who the intended recipient is
    * @param message - The message the sender is sending
    */
-  public constructor(title: string, receiver_name: string, message: string) {
+  public constructor (title: string,
+                      receiver_name: string,
+                      message: string,
+                      code: string = "",
+                      receive_reply: boolean = false,
+                      has_been_read: boolean = false,
+                      time_submitted: Date = new Date()
+                      ) {
     this.verify_string(title, "title");
     this.verify_string(receiver_name, "intended Receiver");
     this.verify_string(message, "message");
+    this.verify_string(code, "code");
     
     this.title = title;
     this.receiver_name = receiver_name;
     this.message = message;
+    this.code = code;
+    this.receive_reply = receive_reply;
+    this.has_been_read = has_been_read;
+    this.time_submitted = time_submitted;
   }
 
   private verify_string(variable_name: string, field_name: string) {
