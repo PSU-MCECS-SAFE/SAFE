390f35ba48ce78b483b1d7552034e355742b3e60
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-04-10 03:42:35 -0700

    Sender class updated
    based on some feedback sender class has been update so the a new message is initialized with a function and not the contractor

--- src/safeSender/SafeSender.ts
+++ src/safeSender/SafeSender.ts
@@ -1,18 +1,47 @@
 export class Sender {
+  //sender class has two objects. the message the sender wants to send and an the database where it is stored
   newMessage: Message;
   database: MessageDB;
 
-  public constructor(title: string, receiver_name: string, message: string) {
-    this.newMessage = new Message(title, receiver_name, message);
+  public constructor() {
+    this.newMessage = null;
     this.database = new MessageDB();
   }
 
-  public savetoDB() {
-    this.database.addMessage(this.message);
+  //create a new message using the necessary strings
+  public createMessage(title: string, receiver_name: string, message: string) {
+    if (this.newMessage === undefined || this.newMessage === null) {
+      this.newMessage = new Message(title, receiver_name, message);
+    } else {
+      throw new Error("A Message already exists.");
+    } 
   }
 
+  //save message to the database
+  public savetoDB() {
+    if (this.newMessage === undefined || this.newMessage === null) {
+      throw new Error("No message to save to the database.");
+    } else {
+      this.database.addMessage(this.newMessage);
+    }
+  }
+
+  //email the message to the receiver
   public emailMessage() {
-    
+    if (this.newMessage === undefined || this.newMessage === null) {
+      throw new Error("No message to email.");
+    } else {
+
+    }
+  }
+
+  //Let the sender View the code connected to the sender's message
+  public viewCode() {
+    if (this.newMessage === undefined || this.newMessage === null) {
+      throw new Error("There is no message or code related to a message.");
+    } else {
+
+    }
   }
 }
 
