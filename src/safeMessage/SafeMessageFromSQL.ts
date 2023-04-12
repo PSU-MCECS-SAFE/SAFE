0cb255b5155b0b081dd0be21d2c8b2dd115bce9f
dylanconklin <dconklin@pdx.edu>
2023-04-12 13:48:55 -0700

    Removed Message class, Implemented new insertMessage and getMessage functions
    Deletes the Message class in favor of using the return value from database queries.
    
    Implemented addMessage function to insert data from users into database.
    
    Implement getMessage function to get list of objects representing messages from database.

--- src/safeMessage/SafeMessageFromSQL.ts
+++ src/safeMessage/SafeMessageFromSQL.ts
@@ -1,49 +1,48 @@
 import Client from 'ts-postgres'
-import Message from './SafeMessage'
 import { ResultRow } from 'ts-postgres/dist/src/result';
+
+function genCode() : string {
+    return "a";
+}
+
+async function insertMessage(title: string, receiver_name: string, message: string){
+    try {
+        await messageDBConnect.connect();
+
+        let val = [title, receiver_name, message, genCode(), "false", "false", Date.now()].join(", ");
+        let query = "INSERT INTO Message VALUES (" + val + ");";
+
+        await messageDBConnect.query(query);
+
+        console.log("Data inserted successfully");
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await messageDBConnect.end();
+    }
+}
 
 /**
  * Connect to Database and run query
  * @param query - string containing query to be made
  */
-async function psql_to_Message(query: string) : Promise<Message[]> {
+async function getMessage(query: string) : Promise<ResultRow<Client.Value>[]>{
     // Connect to DB
     var client = new Client.Client({"host": "localhost"});
     await client.connect();
 
     // Get Query and make each row an element in an array
     var result_iterable = await client.query(query);
-    var msgs = [...result_iterable];
 
     // Close DB
     client.end();
 
-    // Convert DB results to Message
-    return psql_create_Messages(msgs);
-}
+    // Convert object iterable to array of objects
+    var msgs = [...result_iterable];
 
-/**
-* Parser for the `Message` class object from PostgreSQL query
-* @param msgs - PostgreSQL query results in an array
-*/
-function psql_create_Messages(msgs: ResultRow<Client.Value>[]) : Message[] {
-    // Verify there is at least one entry
-    if (msgs.length < 1) {
+    if(msgs.length < 1){
         throw new Error("No messages in database query");
     }
 
-    // Return Message objects from data in DB entry
-    var messages: Message[] = [];
-    for(const msg of msgs){
-        let title = msg.get('title') as string;
-        let receiver_name = msg.get('receiver_name') as string;
-        let message = msg.get('message') as string;
-        let code = msg.get('code') as string;
-        let receive_reply = msg.get('receive_reply') as boolean;
-        let has_been_read = msg.get('has_been_read') as boolean;
-        let time_submitted = msg.get('time_submitted') as Date; // Will probably break, untested
-
-        messages.push(new Message(title, receiver_name, message, code, receive_reply, has_been_read, time_submitted));
-    }
-    return messages;
+    return msgs;
 }
