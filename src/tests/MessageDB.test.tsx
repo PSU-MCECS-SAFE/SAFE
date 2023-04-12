b2e61fa852b748fefff631d85c7e699554c77cef
vincentmask <liangmask@gmail.com>
2023-04-11 20:53:25 -0700

    added addMessage()testing, improved db connection test

--- src/tests/MessageDB.test.tsx
+++ src/tests/MessageDB.test.tsx
@@ -1,19 +1,44 @@
 import { messageDB } from '../safeMessageDB/messageDB';
 import { messageDBConnect } from '../safeMessageDB/messageDBConnect';
 
+
 describe('PostgreSQL database connection', () => {
+    let client: any;
+    let insert = new messageDB();
+
+    beforeAll(async () => {
+        client = await messageDBConnect.connect();
+    });
+
     afterAll(async () => {
-        await messageDBConnect.end();
+        await client.release();
     });
 
     // Message DB connection testing 
-    it('should be able to connect to the database', async () => {
-        try {
-            const client = await messageDBConnect.connect();
-            expect(true).toBeTruthy();
-        } catch (err) {
-            fail(err);
-        }
+    it('should be able to query the "Message" table', async () => {
+        const result = await client.query('SELECT * FROM "Message"');
+        expect(result.rows.length).toBeGreaterThan(0);
     });
 
+
+    // Insert data object to database
+    it('should be able to insert to the the "Message" table', async () => {
+        const message = {
+            title: "Testing",
+            receiver_name: "John",
+            message: "string",
+            code: "",
+            receive_reply: false,
+            has_been_read: false,
+            time_submitted: new Date()
+        };
+        insert.addMessage(message);
+        const result = await client.query(
+            'SELECT * FROM "Message" ORDER BY time_submitted DESC LIMIT 1'
+        );
+        expect(result.rows.length).toBe(1);
+        expect(result.rows[0].title).toEqual("Testing");
+        expect(result.rows[0].receiver_name).toEqual("John");
+        expect(result.rows[0].message).toEqual("string");
+    });
 });
