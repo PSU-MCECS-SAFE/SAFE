45ec2533cb1917d803ac85526c2d0ec238c0f609
imOnVacation <itsmealice257@gmail.com>
2023-04-12 18:43:24 -0700

    removed message class and pass in param since no longer using message class

--- src/safeMessageDB/messageDB.tsx
+++ src/safeMessageDB/messageDB.tsx
@@ -1,13 +1,12 @@
 import { messageDBConnect } from './messageDBConnect';
-import { Message } from './safeMessage/safeMessage';
 
 export class messageDB {
-  public async addMessage(message: Message) {
+  public async addMessage(title: string, receiver_name: string, message: string) {
     try {
       await messageDBConnect.connect();
 
       const query = `INSERT INTO "Message" (title, receiver_name, message, code, receive_reply, has_been_read, time_submitted) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-      const values = [message.title, message.receiver_name, message.message, null, null, null, null];
+      const values = [title, receiver_name, message, null, null, null, null];
 
       await messageDBConnect.query(query, values);
       console.log('Data inserted successfully');
