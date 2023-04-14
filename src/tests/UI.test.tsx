d0de4840a584f760136f6419dcc2ea6b46fceb80
imOnVacation <itsmealice257@gmail.com>
2023-04-13 21:54:22 -0700

    added two tests. Submit btn disabled when required input empty; Display word count of message

--- src/tests/UI.test.tsx
+++ src/tests/UI.test.tsx
@@ -1,3 +1,17 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import { render, screen, fireEvent } from '@testing-library/react';
+import SafeUI from '../safeUI/SafeUI';
+
+test('submit button is disabled when required fields are empty', () => {
+  render(<SafeUI />);
+  const submitButton = screen.getByRole('button', { name: /Submit/i });
+  expect(submitButton).toBeDisabled();
+});
+
+test('displays word count of message', () => {
+  render(<SafeUI />);
+  const messageInput = screen.getByPlaceholderText(/Enter Message/i);
+  fireEvent.change(messageInput, {
+    target: { value: 'This is a test message' },
+  });
+  expect(screen.getByText(/5 words/i)).toBeInTheDocument();
+});
