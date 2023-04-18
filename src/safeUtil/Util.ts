fc8df8e54cf64d257ac3aaaa087d8b14536d434e
Leshi Chen <itsmealice257@gmail.com>
2023-04-17 21:25:47 -0700

    fix error

--- src/safeUtil/Util.ts
+++ src/safeUtil/Util.ts
@@ -1,17 +1,39 @@
-async function SendEmail()
-{
+// async function SendEmail()
+// {
+//     var message = 'This is supposed to be a message from the db';
+//     const { exec } = require('child_process');
+//     //Fill out email address below.
+//     exec(`mail -s 'You have received new feedback!' @pdx.edu <<< "${message}"`,
+//             (error, stdout, stderr) => {
+//                 console.log(stdout);
+//                 console.log(stderr);
+//                 if (error !== null) {
+//                     console.log(`exec error: ${error}`);
+//                 }
+//             });
+// }
+
+// SendEmail()
+// .catch(e => console.log(e));
+
+import { promisify } from 'util';
+import { exec } from 'child_process';
+
+export class util {
+  public async SendEmail() {
     var message = 'This is supposed to be a message from the db';
-    const { exec } = require('child_process');
+    const command = `mail -s 'You have received new feedback!' leshi@pdx.edu <<< "${message}"`;
+    const execPromise = promisify(exec);
+
     //Fill out email address below.
-    exec(`mail -s 'You have received new feedback!' @pdx.edu <<< "${message}"`,
-            (error, stdout, stderr) => {
-                console.log(stdout);
-                console.log(stderr);
-                if (error !== null) {
-                    console.log(`exec error: ${error}`);
-                }
-            });
+    try {
+      const { stdout, stderr } = await execPromise(command);
+      console.log(stdout);
+      console.log(stderr);
+    } catch (error) {
+      console.error(`exec error: ${error}`);
+    }
+  }
 }
 
-SendEmail()
-.catch(e => console.log(e));
+export default util;
