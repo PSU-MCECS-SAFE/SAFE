18a761648eb7aa4c4453f8d51b2bef9ebf9bea98
NAlexH2 <alex@nah-red.net>
2023-04-18 23:14:40 -0700

    Started on tests. Demo how this can work.

--- src/tests/Util.test.tsx
+++ src/tests/Util.test.tsx
@@ -1,3 +1,33 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
+import fs from 'fs';
+import * as util from "../safeUtil/Util";
+import { safeJSONProps as sjp } from "../safeUtil/Util";
+
+
+describe('Util Tests', () => {
+
+    //JSON 
+    describe('Identify each JSON property from test file.', () => {
+        beforeAll(() => {
+            const testJSON = {
+                username: 'Test Name',
+                password: 'testpassword',
+                db_address: 'test.address',
+                db_name: 'test db name',
+                rcvr_email: 'test@testing.com'
+            };
+            const jsonTest = JSON.stringify(testJSON);
+            fs.writeFileSync(TESTPATH, jsonTest);
+        });
+
+        afterAll(() => {
+            fs.unlink('./testing.json', (err) => {
+                if (err) throw err;
+            });
+        });
+
+        const TESTPATH = './testing.json';
+        it('Acquire username from testing.json', () => {
+            expect(util.getConfigProp(sjp.username, TESTPATH)).toBe("Test Name")
+        });
+    });
+});
