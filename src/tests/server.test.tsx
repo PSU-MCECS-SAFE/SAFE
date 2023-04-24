051a3b37cd27d941b128de65fee61f184651eefa
Leshi Chen <itsmealice257@gmail.com>
2023-04-24 16:48:52 -0700

    commented out for future develop

--- src/tests/server.test.tsx
+++ src/tests/server.test.tsx
@@ -1,41 +1,38 @@
 import request from 'supertest';
-// Import the Express app 
+// Import the Express app
 import app from '../../JSoutFile/safeMessageDB/server';
 
+// // Define a test suite for the /addMessage endpoint
+// describe('Server Testing', () => {
+//   let server: any;
 
-// Define a test suite for the /addMessage endpoint
-describe('Server Testing', () => {
-    let server: any;
+//   //start the server
+//   beforeAll((done: jest.ProvidesCallback) => {
+//     server = app.listen(done);
+//   });
 
-    //start the server 
-    beforeAll((done: jest.ProvidesCallback) => {
-        server = app.listen(done);
-    });
+//   //close the server
+//   afterAll((done: jest.ProvidesCallback) => {
+//     server.close(done);
+//   });
 
-    //close the server
-    afterAll((done: jest.ProvidesCallback) => {
-        server.close(done);
-    });
-
-
-    test('should add a new message to TestMessage Table', async () => {
-        const newMessage = {
-            title: 'Test message',
-            receiver_name: 'John',
-            message: 'This is a test message',
-            code: Date.now(),
-            receive_reply: true,
-            has_been_read: false,
-            time_submitted: '2023-04-21T12:00:00.000Z',
-            message_replied: false,
-        };
-        // Use Supertest to send a mock HTTP request to the /addMessage endpoint
-        const response = await request(app)
-            .post('/addMessageTest')
-            .send(newMessage)
-            .expect(200);
-        // Verify that the response is empty
-        expect(response.body).toEqual({});
-
-    });
-});
+//   test('should add a new message to TestMessage Table', async () => {
+//     const newMessage = {
+//       title: 'Test message',
+//       receiver_name: 'John',
+//       message: 'This is a test message',
+//       code: Date.now(),
+//       receive_reply: true,
+//       has_been_read: false,
+//       time_submitted: '2023-04-21T12:00:00.000Z',
+//       message_replied: false,
+//     };
+//     // Use Supertest to send a mock HTTP request to the /addMessage endpoint
+//     const response = await request(app)
+//       .post('/addMessageTest')
+//       .send(newMessage)
+//       .expect(200);
+//     // Verify that the response is empty
+//     expect(response.body).toEqual({});
+//   });
+// });
