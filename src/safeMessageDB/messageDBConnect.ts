1a7f228b2ee17a0658bcee8daefcbc9ce4ad01fa
Leshi Chen <itsmealice257@gmail.com>
2023-04-24 13:02:03 -0700

    change to any type instead of void

--- src/safeMessageDB/messageDBConnect.ts
+++ src/safeMessageDB/messageDBConnect.ts
@@ -1,20 +1,18 @@
 import { Client } from 'pg';
 import {
-    safeJSONProps as sjp,
-    getConfigProp,
-    safeConfigPath as scp
-} from '../safeUtil/Util'
+  safeJSONProps as sjp,
+  getConfigProp,
+  safeConfigPath as scp,
+} from '../safeUtil/Util';
 
-// Create database connection 
-export const messageDBConnect = new Client({
+// Create database connection
+export const messageDBConnect: any = new Client({
+  user: getConfigProp(sjp.username, scp),
+  password: getConfigProp(sjp.password, scp),
+  host: getConfigProp(sjp.db_address, scp),
+  database: getConfigProp(sjp.db_name, scp),
+  port: 5432,
 
-    user: getConfigProp(sjp.username, scp),
-    password: getConfigProp(sjp.password, scp),
-    host: getConfigProp(sjp.db_address, scp),
-    database: getConfigProp(sjp.db_name, scp),
-    port: 5432,
-
-    // only use this option for development purposes
-    ssl: { rejectUnauthorized: false },
+  // only use this option for development purposes
+  ssl: { rejectUnauthorized: false },
 });
-
