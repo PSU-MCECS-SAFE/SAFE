729dae70c4e90e1d68954c813b9cd660675910cb
Leshi Chen <itsmealice257@gmail.com>
2023-04-24 16:51:49 -0700

    list the right order of client constructor

--- src/safeMessageDB/messageDBConnect.ts
+++ src/safeMessageDB/messageDBConnect.ts
@@ -6,13 +6,12 @@ import {
 } from '../safeUtil/Util';
 
 // Create database connection
-export const messageDBConnect: any = new Client({
+export const messageDBConnect: Client = new Client({
   user: getConfigProp(sjp.username, scp),
-  password: getConfigProp(sjp.password, scp),
-  host: getConfigProp(sjp.db_address, scp),
   database: getConfigProp(sjp.db_name, scp),
   port: 5432,
-
+  host: getConfigProp(sjp.db_address, scp),
+  password: getConfigProp(sjp.password, scp),
   // only use this option for development purposes
   ssl: { rejectUnauthorized: false },
 });
