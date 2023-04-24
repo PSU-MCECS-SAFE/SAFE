6e691f022106aa7e98c3d8f15fe8b9c3c43ca4b9
Leshi Chen <itsmealice257@gmail.com>
2023-04-24 13:04:35 -0700

    moved sendMail to server addMessage POST request

--- src/safeUtil/Util.ts
+++ src/safeUtil/Util.ts
@@ -1,37 +1,17 @@
-import { promisify } from 'util';
-import { exec } from 'child_process';
 import fs from 'fs';
 
-export const safeConfigPath = '../../../safeConfig/safeConfig.json'
-
+export const safeConfigPath = '../../../safeConfig/safeConfig.json';
 
 /* Use me when calling getConfigProp! Import like this if you want to use me!
  * import { safeJSONProps as sjp } from "../safeUtil/Util";
  */
 export const enum safeJSONProps {
-  username = "username",
-  password = "password",
-  db_address = "db_address",
-  db_name = "db_name",
-  rcvr_email = "rcvr_email"
+  username = 'username',
+  password = 'password',
+  db_address = 'db_address',
+  db_name = 'db_name',
+  rcvr_email = 'rcvr_email',
 }
-
-
-export async function sendEmail() {
-  var message = 'This is supposed to be a message from the db';
-  const command = `mail -s 'You have received new feedback!' leshi@pdx.edu <<< "${message}"`;
-  const execPromise = promisify(exec);
-
-  //Fill out email address below.
-  try {
-    const { stdout, stderr } = await execPromise(command);
-    console.log(stdout);
-    console.log(stderr);
-  } catch (error) {
-    console.error(`exec error: ${error}`);
-  }
-}
-
 
 /**
  * Get specific information from a json file and its properties.
