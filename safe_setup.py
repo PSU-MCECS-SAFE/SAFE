ca31be76e98164fd6fca2c8ce9509bedf0363c3c
NAlexH2 <alex@nah-red.net>
2023-04-30 22:50:20 -0700

    Ran Black formatter. Good changes.

--- safe_setup.py
+++ safe_setup.py
@@ -11,13 +11,14 @@ def isDebugPresent():
     """
     Detects if the command line had any additional args and if they were the
     debug arg.
-    
-    ### Return: 
+
+    ### Return:
     True if debug arg detected, false otherwise
     """
     if len(sys.argv) == 2 and (sys.argv[1] == "-d" or sys.argv[1] == "-D"):
-            return True
+        return True
     return False
+
 
 """ This is not a docstring, no. This is a tribute!
 
@@ -39,7 +40,6 @@ __CFG_EXISTS = os.path.exists(__CFG_PATH)
 __BUILD_EXISTS = os.path.exists(__BUILD_PATH)
 __JSOUT_EXISTS = os.path.exists(__JSOUT_PATH)
 __DEBUG = isDebugPresent()
-
 
 
 def print_info(username, password, db_address, db_name, rcvr_email):
@@ -70,13 +70,15 @@ def whoIsMyHost():
     and the debug argument is not present for the script, abort mission!
     ### Return:
     False if the regex failed to find matching hostname from the function call.
-    
+
     True otherwise.
     """
     clearScreen()
     myHostIs = subprocess.run(["hostname"], shell=True, stdout=subprocess.PIPE)
-    pattern = r'(ada\.cs\.pdx\.edu|babbage\.cs\.pdx\.edu|quizor\d+\.cs\.pdx\.edu)'
-    if len(re.findall(pattern, myHostIs.stdout.decode('utf-8'))) == 0:
+    pattern = (
+        r"(ada\.cs\.pdx\.edu|babbage\.cs\.pdx\.edu|quizor\d+\.cs\.pdx\.edu)"
+    )
+    if len(re.findall(pattern, myHostIs.stdout.decode("utf-8"))) == 0:
         return False
     return True
 
@@ -167,93 +169,94 @@ def makeConfigFile():
     time.sleep(2)
 
 
-
 def executeNodeJSScripts():
     """
     Perform the NPM script options. This can be added to as the project is passed
     on and changed.
     """
-    print("\n\nNow performing deployment actions. Some directories related\n" + 
-          "the websites code may be removed to generate new versions.\n" + 
-          "\nThis is PERFECTLY NORMAL and TO BE EXPECTED if this script\n" + 
-          "has been ran previously.\n")
+    print(
+        "\n\nNow performing deployment actions. Some directories related\n"
+        + "the websites code may be removed to generate new versions.\n"
+        + "\nThis is PERFECTLY NORMAL and TO BE EXPECTED if this script\n"
+        + "has been ran previously.\n"
+    )
     time.sleep(6)
-    
+
     print("\nThis script will now resume.\n")
     time.sleep(3)
-    
+
     print("\nRunning npm scripts to prepare website for deployment. . .\n")
-    
-    os.system("npm --silent i")        
-    
+
+    os.system("npm --silent i")
+
     # Clean builds never hurt anyone
-    if(__BUILD_EXISTS):
+    if __BUILD_EXISTS:
         print("\nFound old build dir. . .    removing. . .")
         shutil.rmtree(__BUILD_PATH)
     os.system("npm --silent run build")
 
-
     # Lets make the server executable and usable so that the site can
     # properly pass data from the page to the database
-    if(__JSOUT_EXISTS):
+    if __JSOUT_EXISTS:
         print("\nJSoutFile directory found. . .    removing. . .")
         shutil.rmtree(__JSOUT_PATH)
     print("\nCompiling database server code. . .")
     os.system("npx --silent tsc")
 
 
-
 def modifiyUserGroupPermissions():
     """
     Modifies the user group permissions so that the PSU linux systems apache
     server can access the SAFE directories and present the webpage.
-    """ 
+    """
     # If the website fails to display, check these permissions against the
     # CAT's documentation on what permissions must be set for the apache
     # server to access the directories.
     # https://cat.pdx.edu/services/web/account-websites/
-    print("\nUNIX-like system detected. . . Running chmod changes on REQUIRED" +
-          "directories for the SAFE website on PSU servers.")
+    print(
+        "\nUNIX-like system detected. . . Running chmod changes on REQUIRED"
+        + "directories for the SAFE website on PSU servers."
+    )
     time.sleep(1)
     os.system("chmod 711 ../SAFE")
     os.system("chmod -R 711 ./build")
     print("\nModifications complete\n")
     time.sleep(1)
-    
-    
-
-
 
 
 ############ main ############
 def main():
-    
     # If the script is ran on non-unix machines, and -d or -D is not present
     # this message will appear and the script will terminate with sys.exit
     if whoIsMyHost() is False and not __DEBUG:
-        print("This script can only be ran on Portland State University " +
-              "linux servers owned and operated by the CAT.\n" +
-              "If this message is printed in error, the script needs to " +
-              "be updated. Contact the development team for more " + 
-              "support.\n")
+        print(
+            "This script can only be ran on Portland State University "
+            + "linux servers owned and operated by the CAT.\n"
+            + "If this message is printed in error, the script needs to "
+            + "be updated. Contact the development team for more "
+            + "support.\n"
+        )
         sys.exit()
-    
+
     # If being ran with -d or -D, give the user notice
     if __DEBUG:
-        print("\n\nDEBUG MODE DETECTED. . .    SCRIPT WILL CONTINUE IN 3 " + 
-              "SECONDS. . .\n\n")
+        print(
+            "\n\nDEBUG MODE DETECTED. . .    SCRIPT WILL CONTINUE IN 3 "
+            + "SECONDS. . .\n\n"
+        )
         time.sleep(2)
 
     makeConfigFile()
     executeNodeJSScripts()
-    
+
     # Ensure we are on the right type of system for this to actually work.
-    if(__OS_NAME == 'posix'):
+    if __OS_NAME == "posix":
         modifiyUserGroupPermissions()
-    
+
     # We're finished!
     print("\n\nSAFE setup complete!\n\n")
     time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
