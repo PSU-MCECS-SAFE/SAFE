00d4d59dcd7182272be9a52e66df76211124a011
Leshi Chen <99460989+imOnVacation@users.noreply.github.com>
2023-05-03 16:39:50 -0700

    Revert "Jaafar"

--- src/safeUI/Styles/Styled.tsx
+++ src/safeUI/Styles/Styled.tsx
@@ -1,34 +1,19 @@
 // This file is implemented as styling for the submit button with properties instead of using
 // default properties provided by MUI5.
 
-import { Button } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { lightGreen } from "@mui/material/colors";
+import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+// import { lightGreen } from '@mui/material/colors';
 
 export const StyledSubmitButton = styled(Button)(({ theme }) => ({
   backgroundColor: '#6a7f10',
-  fontSize: "16px",
-  borderRadius: "4px",
-  padding: "0.5em 2em",
-  boxShadow: "none",
-  transition: "all 0.3s ease",
-  "&:hover": {
-    backgroundColor: lightGreen[700],
-    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
-  },
-}));
-
-export const StyledButton = styled(Button)(({ theme }) => ({
-  color: "#6a7f10",
-  fontSize: "16px",
-  borderRadius: "4px",
-  padding: "0.5em 2em",
-  boxShadow: "none",
-  transition: "all 0.3s ease",
-  border: "none",
-  "&:hover": {
-    backgroundColor: lightGreen[700],
-    color: "#FFFFFF",
-    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+  fontSize: '16px',
+  borderRadius: '4px',
+  padding: '0.5em 2em',
+  boxShadow: 'none',
+  transition: 'all 0.3s ease',
+  '&:hover': {
+    backgroundColor: '#2e8b57',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
   },
 }));
