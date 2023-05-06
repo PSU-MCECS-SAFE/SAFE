0ccaff8f02c95963583d2a958b60cf11453923d1
Jeff McHale <jeffmchale94@gmail.com>
2023-05-05 17:08:01 -0700

    Reworked Banner Location

---
+++ src/safeReceiverDashboard/Components/bannerNavBar.tsx
@@ -0,0 +1,71 @@
+import { Box, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import { useRef, useState } from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+
+function BannerNavBar() {
+  const [open, setOpen] = useState(false);
+  const targetRef = useRef(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#6a7f10',
+        height: '38px',
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: '25px',
+        paddingRight: '25px',
+        justifyContent: 'space-between',
+      }}
+    >
+      <img
+        src="/PSU_logo_accent_transparent.png"
+        alt="Logo"
+        width="135"
+        height="53"
+      />
+      <Typography
+        sx={{
+          color: 'black',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          fontSize: '24px',
+        }}
+      >
+        Safe Faculty Dashboard
+      </Typography>
+      <IconButton id="logout-button" onClick={handleClick} ref={targetRef}>
+        <PersonIcon />
+      </IconButton>
+      <Menu
+        id="logout-menu"
+        open={open}
+        onClose={handleClose}
+        anchorEl={targetRef.current}
+      >
+        <MenuItem // Logout menu item
+          onClick={() => {
+            handleClose();
+            //Place logout function here
+          }}
+        >
+          {' '}
+          Logout{' '}
+        </MenuItem>
+        <MenuItem onClick={handleClose}> Action2 </MenuItem>
+        <MenuItem onClick={handleClose}> Action3 </MenuItem>
+        {/* What should the other actions be? Do we need them? */}
+      </Menu>
+    </Box>
+  );
+}
+
+export default BannerNavBar;
--- src/safeReceiverDashboard/safeReceiverDashboard.tsx
+++ src/safeReceiverDashboard/safeReceiverDashboard.tsx
@@ -1,81 +1,12 @@
-import {
-  Box,
-  Grid,
-  IconButton,
-  Link,
-  Menu,
-  MenuItem,
-  Snackbar,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 import { useCallback, useEffect, useState } from 'react';
-import PersonIcon from '@mui/icons-material/Person';
+import BannerNavBar from './Components/bannerNavBar';
 
 function ReceiverDashboard() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <Box sx={{ backgroundColor: '#faf7e1', width: 'auto', height: 'auto' }}>
-      <Box
-        sx={{
-          backgroundColor: '#6a7f10',
-          height: '38px',
-          display: 'flex',
-          alignItems: 'center',
-          paddingLeft: '25px',
-          paddingRight: '25px',
-          justifyContent: 'space-between',
-        }}
-      >
-        <img
-          src="/PSU_logo_accent_transparent.png"
-          alt="Logo"
-          width="135"
-          height="53"
-        />
-        <Typography
-          sx={{
-            color: 'black',
-            marginRight: 'auto',
-            marginLeft: 'auto',
-            fontSize: '24px',
-          }}
-        >
-          Safe Faculty Dashboard
-        </Typography>
-        <IconButton id="logout-button" onClick={handleClick}>
-          <PersonIcon />
-        </IconButton>
-        <Menu
-          id="logout-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem // Logout menu item
-            onClick={() => {
-              handleClose();
-              //Place logout function here
-            }}
-          >
-            {' '}
-            Logout{' '}
-          </MenuItem>
-          <MenuItem onClick={handleClose}> Action2 </MenuItem>
-          <MenuItem onClick={handleClose}> Action3 </MenuItem>
-          {/* What should the other actions be? Do we need them? */}
-        </Menu>
-      </Box>
+      <BannerNavBar />
     </Box>
   );
 }
