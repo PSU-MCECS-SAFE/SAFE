c767519e58236b9914d692e0672a0a3cfb13f982
Dylan Conklin <dconklin@pdx.edu>
2023-05-07 02:36:53 -0700

    HUGE Revamp of Code Generator
    Major upgrade for the code generator.
    
    Rewrote most of it to make the code make more sense, and reduce complexity.
    
    Fixed bugs relating to database queries.
    
    Added Support for Expiring Codes.

--- src/safeUtil/generateCode.ts
+++ src/safeUtil/generateCode.ts
@@ -1,41 +1,14 @@
-// import Client from 'ts-postgres';
-// import { ResultRow } from 'ts-postgres/dist/src/result';
-import { messageDBConnect } from '../safeMessageDB/messageDBConnect';
 import QRCode from 'qrcode';
-import { Pool, PoolClient, QueryResult } from 'pg';
+import { PoolClient, QueryResult } from 'pg';
 
 export class Code {
-
-    private static sortChars(str: string): string {
-        let arr: string[] = [];
-        for (let i: number = 0; i < str.length; i++) {
-            arr.push(str[i]);
-        }
-        return arr.sort().join('');
-    }
-
-    private static async isAvailable(client: PoolClient, code: string): Promise<boolean> {
-        var isAvailable: boolean = true;
-        // Check if code exists in database. Get "next" code if it exists in the database
-        const query = {
-            rowMode: 'array',
-            text: 'SELECT $1 FROM "Message" WHERE code = $2;',
-            values: ['time_submitted', code],
-        }
-        var result: QueryResult<any> = await client.query(query);
-        // console.log(result);
-        // if (result.rowCount !== 0 || Code.isExpired()) {}
-        if (result.rowCount !== 0) {
-            isAvailable = false;
-        }
-        return isAvailable;
-    }
 
     public static async genCode(client: PoolClient): Promise<string> {
         const max: number = 64;
         const min: number = 32;
-        const code_length = Code.randInt(max, min);
+        var code_length: number = Code.randInt(max, min);
         const dictionary: string = Code.sortChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+
 
         // Generate random code
         let code_combination: number[] = [];
@@ -43,67 +16,82 @@ export class Code {
             code_combination.push(Code.randInt(dictionary.length - 1));
         }
 
-        var isAvailable: boolean = await Code.isAvailable(client, Code.renderCode(code_combination, dictionary));
-        if (!isAvailable) {
-            // Get "next" possible code
-            code_combination = await Code.getNextCode(client, code_combination, code_length, dictionary, max, min);
-        }
+        await Code.getCode(client, code_combination, dictionary);
 
         return Code.renderCode(code_combination, dictionary);
     }
 
-    private static async checkCombinations(client: PoolClient, code_combination: number[], code_length: number, index: number, dictionary: string): Promise<number[]> {
-        const code = Code.renderCode(code_combination.slice(0, code_combination.length - 2), dictionary);
-
-        const query = {
-            rowMode: 'array',
-            text: 'SELECT code FROM "Message" WHERE code LIKE $1 AND char_length(code) = $2',
-            values: [code, code_combination.length],
-        }
-        var result: QueryResult<any> = await client.query(query);
-
-        let existing_codes: string[] = [];
-        console.log(result.rows);
-        for (let row of result.rows) {
-            existing_codes.push(String(row.code));
-        }
-        // In the loop, we need to increment curr by 1, because we know that the code combination with curr isn't valid
-        // All this does is cycle through every value in the dictionary until it reaches the original value
-        let init: number = code_combination[index];
-        let i: number = (init !== (dictionary.length - 1)) ? (init + 1) : 0;
-        code_combination[index] = i;
-        do {
-            code_combination[index] = i;
-            const renderedCode = Code.renderCode(code_combination, dictionary);
-            if (!existing_codes.includes(renderedCode)) {
-                return code_combination;
-            }
-            i = (i !== (dictionary.length - 1)) ? (i + 1) : 0;
-        } while (i !== init);
-
-        // Returns either a new, available code, or the original code if none is available
-        return code_combination;
-    }
-
-    // TODO: Clean up this code, it could definitely be combined with checkCombinations somehow
-    private static async getNextCode(client: PoolClient, code_combination: number[], code_length: number, dictionary: string, max: number, min: number, index: number = 0): Promise<number[]> {
-        if (index === code_length - 1) {
+    private static async getCode(client: PoolClient, code_combination: number[], dictionary: string, index: number = 0): Promise<boolean> {
+        let isAvailable: boolean = false;
+        if (index === code_combination.length - 1) {
             // We are at the last index in the array
-            await Code.checkCombinations(client, code_combination, code_length, index, dictionary);
+            isAvailable = await Code.queryCodes(client, code_combination, dictionary);
         } else {
             let init: number = code_combination[index];
             let i: number = (init !== (dictionary.length - 1)) ? (init + 1) : 0;
             do {
-                let new_code_combination: number[] = await Code.getNextCode(client, code_combination, code_length, dictionary, max, min, index + 1);
-                if (code_combination !== new_code_combination) {
-                    code_combination = new_code_combination;
+                isAvailable = await Code.getCode(client, code_combination, dictionary, index + 1);
+                if (isAvailable) {
                     break;
                 }
                 i = (i !== (dictionary.length - 1)) ? (i + 1) : 0;
                 code_combination[index] = i;
             } while (i !== init);
         }
-        return code_combination;
+        return isAvailable;
+    }
+
+    private static async queryCodes(client: PoolClient, code_combination: number[], dictionary: string): Promise<boolean> {
+        var isAvailable: boolean = false;
+        const query = {
+            rowMode: 'array',
+            text: 'SELECT code, time_submitted FROM "Message" WHERE code LIKE $1 AND char_length(code) = $2;',
+            values: [String(Code.renderCode(code_combination, dictionary).slice(0, code_combination.length - 1) + "%"), code_combination.length],
+        }
+        var queryResult: QueryResult<any> = await client.query(query);
+        isAvailable = (queryResult.rowCount === 0) ? true : await Code.checkCombinations(client, queryResult, code_combination, dictionary);
+        return isAvailable;
+    }
+
+    private static async checkCombinations(client: PoolClient, queryResult: QueryResult<any>, code_combination: number[], dictionary: string): Promise<boolean> {
+        var isAvailable: boolean = false;
+        var existing_codes: string[] = [];
+        var existing_code_dates: Date[] = [];
+        for(var row: number = 0; row < queryResult.rowCount; row++){
+            existing_codes.push(queryResult.rows[row][0]);
+            existing_code_dates.push(new Date(queryResult.rows[row][1]));
+        }
+        for(var i: number = 0; i < dictionary.length; i++){
+            code_combination[code_combination.length - 1] = i;
+            if(await Code.isAvailable(client, code_combination, dictionary, existing_codes, existing_code_dates)){
+                isAvailable = true;
+                break;
+            }
+        }
+        return isAvailable;
+    }
+
+    private static async isAvailable(client: PoolClient, code_combination: number[], dictionary: string, existing_codes: string[], existing_code_dates: Date[]): Promise<boolean> {
+        var isAvailable: boolean = false;
+        if(!existing_codes.includes(Code.renderCode(code_combination, dictionary))){
+            isAvailable = true;
+        }else if ((new Date().getFullYear() - existing_code_dates[existing_codes.findIndex(e => e === Code.renderCode(code_combination, dictionary))].getFullYear()) > 10){
+            isAvailable = true;
+            const query = {
+                rowMode: 'array',
+                text: 'UPDATE "Message" SET code = NULL WHERE code = $1;',
+                values: [String(Code.renderCode(code_combination, dictionary))],
+            }
+            await client.query(query);
+        }
+        return isAvailable;
+    }
+
+    private static randInt(max: number, min: number = 0) {
+        // Make sure min and max are integers
+        max = Math.floor(max + 1);
+        min = Math.floor(min);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     private static renderCode(code_combination: number[], dictionary: string): string {
@@ -114,20 +102,12 @@ export class Code {
         return code;
     }
 
-    // TODO: Doesn't work for some reason?
-    private static renderCodeCombination(code: string, dictionary: string): number[] {
-        let code_combination: number[] = [];
-        for(var i: number = 0; i < code.length; i++){
-            code_combination.push(dictionary.indexOf(code[i]));
+    private static sortChars(str: string): string {
+        let arr: string[] = [];
+        for (let i: number = 0; i < str.length; i++) {
+            arr.push(str[i]);
         }
-        return code_combination;
-    }
-
-    private static randInt(max: number, min: number = 0) {
-        // Make sure min and max are integers
-        max = Math.floor(max + 1);
-        min = Math.floor(min);
-        return Math.floor(Math.random() * (max - min)) + min;
+        return arr.sort().join('');
     }
 
     public static genURL(page: string, code: string): string {
