66c523b8f398c81eb77ca4c2c9a99ad1ee628f65
NAlexH2 <alex@nah-red.net>
2023-05-13 18:51:01 -0700

    Adding and clarifying comments

--- src/safeUtil/Util.ts
+++ src/safeUtil/Util.ts
@@ -1,6 +1,15 @@
 import fs from 'fs';
 import * as path from 'path';
 
+/* This const is required to be done this way because of how getting file
+ * paths can be, especially when interfacing from the client to the schools
+ * network.
+ * 
+ * Remember, the website is all client ran and computed, but when requesting
+ * resources from the network, that must be done by the machine hosting the
+ * service.
+ * - Alex (Capstone team Spring '23)
+ */
 export const safeConfigPath = path.join(
   __dirname,
   '..',
@@ -10,7 +19,8 @@ export const safeConfigPath = path.join(
   'safeConfig.json'
 );
 
-/* Use me when calling getConfigProp! Import like this if you want to use me!
+/**
+ * Use me when calling getConfigProp! Import like this if you want to use me!
  * import { safeJSONProps as sjp } from "../safeUtil/Util";
  */
 export const enum safeJSONProps {
@@ -22,7 +32,8 @@ export const enum safeJSONProps {
 }
 
 /**
- * Get specific information from a json file and its properties.
+ * Get specific information from a json file and its properties, 
+ * known as a `prop`.
  * @param prop From enum config_props. Used as a key to id which json property
  * the code is hunting for
  * @returns a string containing the information being requested based on prop
