56bb2919451d7410d0972daddaef2d26a4111083
MinhDucMa <ducminh@pdx.edu>
2023-05-18 01:05:05 -0700

    Dynamically resize header text based on screen size

--- src/safeUI/Components/headerText.tsx
+++ src/safeUI/Components/headerText.tsx
@@ -1,5 +1,5 @@
-import { styled, Typography, useMediaQuery } from '@mui/material';
-import React from 'react';
+import { styled, Typography, useMediaQuery } from "@mui/material";
+import React from "react";
 
 // const responsiveFonts = styled(Typography)(({ theme }) => ({
 //     textAlign: 'center',
@@ -13,14 +13,21 @@ function HeaderText() {
   // const smallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
   return (
     <div>
-      <Typography mt={2} mb={3} variant="h3" align="center" gutterBottom>
+      <Typography
+        mt={2}
+        mb={3}
+        variant="h3"
+        align="center"
+        gutterBottom
+        sx={{ fontSize: { xs: 20, sm: 25, md: 30 } }}
+      >
         Welcome to <b>SAFE</b>
         <br />
         PSU's CS Department Anonymous Feedback System
       </Typography>
 
       <Typography mt={2} mb={3} align="center">
-        Find out how we are committed to keeping your identity{' '}
+        Find out how we are committed to keeping your identity{" "}
         <a href="./about.html">anonymous</a>!
       </Typography>
     </div>
