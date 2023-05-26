51577e0c75e59878f9893c8583c182da8adf3c85
Leshi Chen <itsmealice257@gmail.com>
2023-05-26 15:03:47 -0700

    Merged checkString and checkProfanities; Added Docstring to checkString
    Merged checkString and checkProfanities, and used error handling for simplicity and scalability.

--- src/safeUtil/generateCode.ts
+++ src/safeUtil/generateCode.ts
@@ -1,172 +1,252 @@
-import QRCode from 'qrcode';
-import { PoolClient, QueryResult } from 'pg';
+import { PoolClient, QueryResult } from "pg";
 
-export class Code {
+export default class Code {
+  /**
+   * Get a unique code correlating to a response in the database.
+   * @param client The database to connect to
+   * @param table The table to query
+   * @param code Optional pre-defined code to use
+   * @returns A unique code correlating to a response in the database
+   */
+  public static async genCode(
+    client: PoolClient,
+    table: string = "Message",
+    code: string = ""
+  ): Promise<string> {
+    const max_code_length: number = 64;
+    const min_code_length: number = 32;
+    // The dictionary is the character set for generating codes
+    const dictionary: string =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        .split("")
+        .sort()
+        .join("");
 
-    /**
-     * Get a unique code correlating to a response in the database.
-     * @param client The database to connect to
-     * @param table The table to query
-     * @param code Optional pre-defined code to use
-     * @returns A unique code correlating to a response in the database
-     */
-    public static async genCode(client: PoolClient, table: string = "Message", code: string = ""): Promise<string> {
-        const max_code_length: number = 64;
-        const min_code_length: number = 32;
-        const dictionary: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("").sort().join("");
+    // Generate random code, or use the one provided
+    let code_combination: number[] =
+      code.length < min_code_length || code.length > max_code_length
+        ? this.initCodeCombination(
+            this.randInt(max_code_length, min_code_length),
+            dictionary
+          )
+        : this.renderCodeCombination(code, dictionary);
 
+    // Verify random code is available, or get new code instead
+    await this.getCode(client, table, code_combination, dictionary);
 
-        // Generate random code
-        let code_combination: number[] = ((code.length < min_code_length) || (code.length > max_code_length)) ? Code.initCodeCombination(Code.randInt(max_code_length, min_code_length), dictionary) : Code.renderCodeCombination(code, dictionary);
+    return this.renderCode(code_combination, dictionary);
+  }
 
-        await Code.getCode(client, table, code_combination, dictionary);
-
-        return Code.renderCode(code_combination, dictionary);
+  /**
+   * Generate random initial code to use
+   * @param code_length Length of the code to be generated
+   * @param dictionary The character set to use
+   * @returns A random code combination
+   */
+  private static initCodeCombination(
+    code_length: number,
+    dictionary: string
+  ): number[] {
+    let code_combination = [];
+    for (let i: number = 0; i < code_length; i++) {
+      // Insert random character from dictionary
+      code_combination.push(this.randInt(dictionary.length - 1));
     }
+    return code_combination;
+  }
 
-    /**
-     * Generate random initial code to use
-     * @param code_length Length of the code to be generated
-     * @param dictionary The character set to use
-     * @returns A random code combination
-     */
-    private static initCodeCombination(code_length: number, dictionary: string) : number[] {
-        let code_combination = [];
-        for (let i: number = 0; i < code_length; i++) {
-            code_combination.push(Code.randInt(dictionary.length - 1));
+  /**
+   * Iterate through code combination until a code is available
+   * @param client The database to connect to
+   * @param table The table to query
+   * @param code_combination code combination being evaluated
+   * @param dictionary The character set to use
+   * @param index Placement in the code combination
+   * @returns true if the code is available, otherwise false
+   */
+  private static async getCode(
+    client: PoolClient,
+    table: string,
+    code_combination: number[],
+    dictionary: string,
+    index: number = 0
+  ): Promise<boolean> {
+    let isAvailable: boolean = false;
+    if (index === code_combination.length - 1) {
+      // We are at the last index in the array
+      isAvailable = await this.queryCodes(
+        client,
+        table,
+        code_combination,
+        dictionary
+      );
+    } else {
+      // init is the value of the character at this index value
+      let init: number = code_combination[index];
+      // init is the value of the next character in the dictionary, after init
+      let i: number = init !== dictionary.length - 1 ? init + 1 : 0;
+
+      // Cycle through all the characters in the dictionary
+      do {
+        isAvailable = await this.getCode(
+          client,
+          table,
+          code_combination,
+          dictionary,
+          index + 1
+        );
+        if (isAvailable) {
+          break;
         }
-        return code_combination;
+        i = i !== dictionary.length - 1 ? i + 1 : 0;
+        code_combination[index] = i;
+      } while (i !== init);
+    }
+    return isAvailable;
+  }
+
+  /**
+   * Query table and iterate through code combinations to find available codes
+   * @param client The database to connect to
+   * @param table The table to query
+   * @param code_combination code combination being evaluated
+   * @param dictionary The character set to use
+   * @returns true if the code is available, otherwise false
+   */
+  private static async queryCodes(
+    client: PoolClient,
+    table: string,
+    code_combination: number[],
+    dictionary: string
+  ): Promise<boolean> {
+    var isAvailable: boolean = false;
+    var existing_codes: string[] = [];
+    var existing_code_dates: Date[] = [];
+
+    const query = {
+      rowMode: "array",
+      text: "SELECT code, time_submitted FROM $1 WHERE code LIKE $2 AND char_length(code) = $3;",
+      values: [
+        table,
+        String(
+          this.renderCode(code_combination, dictionary).slice(
+            0,
+            code_combination.length - 1
+          ) + "%"
+        ),
+        code_combination.length,
+      ],
+    };
+    var queryResult: QueryResult<any> = await client.query(query);
+
+    if (queryResult.rowCount === 0) {
+      isAvailable = true;
+    } else {
+      for (var row: number = 0; row < queryResult.rowCount; row++) {
+        existing_codes.push(queryResult.rows[row][0]);
+        existing_code_dates.push(new Date(queryResult.rows[row][1]));
+      }
     }
 
-    /**
-     * Iterate through code combination until a code is available
-     * @param client The database to connect to
-     * @param table The table to query
-     * @param code_combination code combination being evaluated
-     * @param dictionary The character set to use
-     * @param index Placement in the code combination
-     * @returns true if the code is available, otherwise false
-     */
-    private static async getCode(client: PoolClient, table: string, code_combination: number[], dictionary: string, index: number = 0): Promise<boolean> {
-        let isAvailable: boolean = false;
-        if (index === code_combination.length - 1) {
-            // We are at the last index in the array
-            isAvailable = await Code.queryCodes(client, table, code_combination, dictionary);
-        } else {
-            let init: number = code_combination[index];
-            let i: number = (init !== (dictionary.length - 1)) ? (init + 1) : 0;
-            do {
-                isAvailable = await Code.getCode(client, table, code_combination, dictionary, index + 1);
-                if (isAvailable) {
-                    break;
-                }
-                i = (i !== (dictionary.length - 1)) ? (i + 1) : 0;
-                code_combination[index] = i;
-            } while (i !== init);
-        }
-        return isAvailable;
+    for (var i: number = 0; i < dictionary.length && !isAvailable; i++) {
+      code_combination[code_combination.length - 1] = i;
+      isAvailable = await this.isAvailable(
+        client,
+        table,
+        code_combination,
+        dictionary,
+        existing_codes,
+        existing_code_dates
+      );
     }
 
-    /**
-     * Query table and iterate through code combinations to find available codes
-     * @param client The database to connect to
-     * @param table The table to query
-     * @param code_combination code combination being evaluated
-     * @param dictionary The character set to use
-     * @returns true if the code is available, otherwise false
-     */
-    private static async queryCodes(client: PoolClient, table: string, code_combination: number[], dictionary: string): Promise<boolean> {
-        var isAvailable: boolean = false;
-        var existing_codes: string[] = [];
-        var existing_code_dates: Date[] = [];
+    return isAvailable;
+  }
 
-        const query = {
-            rowMode: 'array',
-            text: 'SELECT code, time_submitted FROM $1 WHERE code LIKE $2 AND char_length(code) = $3;',
-            values: [table, String(Code.renderCode(code_combination, dictionary).slice(0, code_combination.length - 1) + "%"), code_combination.length],
-        }
-        var queryResult: QueryResult<any> = await client.query(query);
-
-        if (queryResult.rowCount === 0) {
-            isAvailable = true;
-        }else{
-            for(var row: number = 0; row < queryResult.rowCount; row++){
-                existing_codes.push(queryResult.rows[row][0]);
-                existing_code_dates.push(new Date(queryResult.rows[row][1]));
-            }
-        }
-
-        for(var i: number = 0; i < dictionary.length && !isAvailable; i++){
-            code_combination[code_combination.length - 1] = i;
-            isAvailable = await Code.isAvailable(client, table, code_combination, dictionary, existing_codes, existing_code_dates);
-        }
-
-        return isAvailable;
+  /**
+   * Query table and iterate through code combinations to find available codes
+   * @param client The database to connect to
+   * @param table The table to query
+   * @param code_combination code combination being evaluated
+   * @param dictionary The character set to use
+   * @param existing_codes A list of codes that exist in the database similar to the given code combination
+   * @param existing_code_dates List of timestamps for codes in existing_codes
+   * @returns true if the code is available, otherwise false
+   */
+  private static async isAvailable(
+    client: PoolClient,
+    table: string,
+    code_combination: number[],
+    dictionary: string,
+    existing_codes: string[],
+    existing_code_dates: Date[]
+  ): Promise<boolean> {
+    var isAvailable: boolean = false;
+    if (
+      !existing_codes.includes(this.renderCode(code_combination, dictionary))
+    ) {
+      isAvailable = true;
+    } else if (
+      new Date().getFullYear() -
+        existing_code_dates[
+          existing_codes.findIndex(
+            (e) => e === this.renderCode(code_combination, dictionary)
+          )
+        ].getFullYear() >
+      10
+    ) {
+      isAvailable = true;
+      const query = {
+        rowMode: "array",
+        text: "UPDATE $1 SET code = NULL WHERE code = $2;",
+        values: [table, String(this.renderCode(code_combination, dictionary))],
+      };
+      await client.query(query);
     }
-    /**
-     * Query table and iterate through code combinations to find available codes
-     * @param client The database to connect to
-     * @param table The table to query
-     * @param code_combination code combination being evaluated
-     * @param dictionary The character set to use
-     * @param existing_codes A list of codes that exist in the database similar to the given code combination
-     * @param existing_code_dates List of timestamps for codes in existing_codes
-     * @returns true if the code is available, otherwise false
-     */
-    private static async isAvailable(client: PoolClient, table: string, code_combination: number[], dictionary: string, existing_codes: string[], existing_code_dates: Date[]): Promise<boolean> {
-        var isAvailable: boolean = false;
-        if(!existing_codes.includes(Code.renderCode(code_combination, dictionary))){
-            isAvailable = true;
-        }else if ((new Date().getFullYear() - existing_code_dates[existing_codes.findIndex(e => e === Code.renderCode(code_combination, dictionary))].getFullYear()) > 10){
-            isAvailable = true;
-            const query = {
-                rowMode: 'array',
-                text: 'UPDATE $1 SET code = NULL WHERE code = $2;',
-                values: [table, String(Code.renderCode(code_combination, dictionary))],
-            }
-            await client.query(query);
-        }
-        return isAvailable;
-    }
+    return isAvailable;
+  }
 
-    /**
-     * Generate random number within a range
-     * @param max The highest possible number
-     * @param min The lowest possible number, default is 0
-     * @returns A random number between max and min (inclusive)
-     */
-    private static randInt(max: number, min: number = 0): number {
-        // Make sure min and max are integers
-        max = Math.floor(max + 1);
-        min = Math.floor(min);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+  /**
+   * Generate random number within a range
+   * @param max The highest possible number
+   * @param min The lowest possible number, default is 0
+   * @returns A random number between max and min (inclusive)
+   */
+  private static randInt(max: number, min: number = 0): number {
+    // Make sure min and max are integers
+    max = Math.floor(max + 1);
+    min = Math.floor(min);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-    /**
-     * Convert code combination to code based on dictionary
-     * @param code_combination code combination being evaluated
-     * @param dictionary The character set to use
-     * @returns The code correlating to the code combination
-     */
-    private static renderCode(code_combination: number[], dictionary: string): string {
-        let code: string = "";
-        code_combination.forEach(char => {
-            code += (dictionary[char]);
-        });
-        return code;
-    }
+  /**
+   * Convert code combination to code based on dictionary
+   * @param code_combination code combination being evaluated
+   * @param dictionary The character set to use
+   * @returns The code correlating to the code combination
+   */
+  private static renderCode(
+    code_combination: number[],
+    dictionary: string
+  ): string {
+    let code: string = "";
+    code_combination.forEach((char) => {
+      code += dictionary[char];
+    });
+    return code;
+  }
 
-     /**
-      * Convert code to code combination based on dictionary
-      * @param code code being evaluated
-      * @param dictionary The character set to use
-      * @returns The code combination correlating to the code
-      */
-    private static renderCodeCombination(code: string, dictionary: string) {
-        let code_combination = [];
-        for(var i = 0; i < code.length; i++){
-            code_combination.push(dictionary.indexOf(code[i]));
-        }
-        return code_combination;
+  /**
+   * Convert code to code combination based on dictionary
+   * @param code code being evaluated
+   * @param dictionary The character set to use
+   * @returns The code combination correlating to the code
+   */
+  private static renderCodeCombination(code: string, dictionary: string) {
+    let code_combination = [];
+    for (var i = 0; i < code.length; i++) {
+      code_combination.push(dictionary.indexOf(code[i]));
     }
+    return code_combination;
+  }
 }
--- src/tests/Code.test.tsx
+++ src/tests/Code.test.tsx
@@ -1,4 +1,44 @@
-import { Code } from "../safeUtil/generateCode";
+import Code from "../safeUtil/generateCode";
+import { PoolClient } from 'pg';
+import { messageDBConnect } from '../safeMessageDB/messageDBConnect';
 
-// test('testName', () => {});
-test('testname', () => {});
+describe("Code Generation", () => {
+  const dictionary: string =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+      .split("")
+      .sort()
+      .join("");
+
+  // test('testName', () => {});
+  test("Sorting Dictionary", () => {
+    expect(dictionary).toBe(
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    );
+  });
+
+  test("Rendering Code and Rendering Code Combinations are Inverses", () => {
+    expect(
+      // @ts-ignore
+      Code.renderCode([...Array(dictionary.length).keys()], dictionary)
+    ).toStrictEqual(dictionary);
+
+    // @ts-ignore
+    expect(Code.renderCodeCombination(dictionary, dictionary)).toStrictEqual([
+      ...Array(dictionary.length).keys(),
+    ]);
+  });
+
+  test("Random Number Generator", () => {
+    // @ts-ignore
+    expect(Code.randInt(0)).toBe(0);
+    // @ts-ignore
+    expect(Code.randInt(0, 0)).toBe(0);
+    // @ts-ignore
+    expect(Code.randInt(dictionary.length, dictionary.length)).toBe(dictionary.length);
+
+    // @ts-ignore
+    expect(Code.randInt(dictionary.length, 10)).toBeGreaterThanOrEqual(10);
+    // @ts-ignore
+    expect(Code.randInt(dictionary.length, 10)).toBeLessThanOrEqual(dictionary.length);
+  });
+});
