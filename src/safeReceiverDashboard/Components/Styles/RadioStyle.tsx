69917dd9378d388201c62aafae95e745e429d952
Leshi Chen <itsmealice257@gmail.com>
2023-05-29 12:35:59 -0700

    remove non-used file, confirmed with Minh

--- src/safeReceiverDashboard/Components/Styles/RadioStyle.tsx
+++
@@ -1,7 +0,0 @@
-import { Radio } from "@mui/material";
-import { styled } from "@mui/material/styles";
-// import { lightGreen } from '@mui/material/colors';
-
-export const StyledRadioButton = styled(Radio)(({ theme }) => ({
-  borderRadius: 0,
-}));
